e18eca31b5b9938865382f6f5f9b6283d2036632
Pyrogue <pyrogue6@gmail.com>
2017-03-27 20:57:30 -0400

    Giving up for a bit to learn python more deeply... Onto another project with me. RIP project deadlines. :'(

--- HWM.py
+++ HWM.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.4
 #google hangouts bot targeted at the Raspberry Pi
 #documentation: https://hangups.readthedocs.io/en/stable/
 #extra reading: http://dabeaz.com/coroutines/
 
 import hangups
+import asyncio	#for this future stuff... wow i have a lot to learn
 
 tokenFile = "token"
 
@@ -35,10 +36,63 @@ print("------------")
 user = hangups.build_user_conversation_list(me)
 print(user)
 
-print(user.next())
+# print(user.next())
 # for u in user:
 # 	print(u)
 #print(users.get_all())
+
+
+# for i in hangups.build_user_conversation_list(me):
+# 	conversation_list = i
+#-----------------------------------------------------------
+# loop = asyncio.get_event_loop()
+# task = loop.create_task(hangups.build_user_conversation_list(me))
+# (ul,cl) = loop.run_until_complete(task)
+# print()
+# print("USER LIST:")
+# print(ul)
+# print("CONVERSATION LIST:")
+# print(cl)
+# print()
+# USER LIST:
+# <hangups.user.UserList object at 0x7f425db7a400>
+# CONVERSATION LIST:
+# <hangups.conversation.ConversationList object at 0x7f425dbb1390>
+#-----------------------------------------------------------------
+
+
+# user_list = [hangups.build_user_conversation_list(me)]
+# print(hangups.build_user_conversation_list.__code__)
+# user_list, conversation_list = (
+#     hangups.build_user_conversation_list(me)
+# )
+
+# request = hangups.hangouts_pb2.GetSuggestedEntitiesRequest(
+#         request_header=client.get_request_header(),
+#         max_count=100,
+# query_presence(query_presence_request)
+
+#copy-pasta from docs... i am so lost
+# def sync_recent_conversations(client, _):
+#     user_list, conversation_list = (
+#         yield from hangups.build_user_conversation_list(client)
+#     )
+#     all_users = user_list.get_all()
+#     all_conversations = conversation_list.get_all(include_archived=True)
+
+#     print('{} known users'.format(len(all_users)))
+#     for user in all_users:
+#         print('    {}: {}'.format(user.full_name, user.id_.gaia_id))
+
+#     print('{} known conversations'.format(len(all_conversations)))
+#     for conversation in all_conversations:
+#         if conversation.name:
+#             name = conversation.name
+#         else:
+#             name = 'Unnamed conversation ({})'.format(conversation.id_)
+#         print('    {}'.format(name))
+
+# sync_recent_conversations(me,"pyrogue6")
 
 #disconnect
 me.disconnect()
